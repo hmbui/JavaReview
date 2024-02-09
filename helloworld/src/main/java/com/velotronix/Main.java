7182a83f805a00cf7e6b300ad61aaee2717fa77d
Hai Bui <haib@vmware.com>
2024-02-08 21:56:55 -0800

    Adding the Main class.

---
+++ helloworld/src/main/java/com/velotronix/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Hello World! Simple App.
+ * 
+ * Author: Hai Bui
+ * Created: Feb 8, 2024
+ */
+
+ package com.velotronix;
+
+
+ /**
+  * Main Class
+  */
+ public class Main {
+     /**
+      * Print out the message "Hello World!" to the console.
+      * @param args
+      */
+     public static void main(String[] args) {
+         System.out.println("Hello World!");
+     }
+ }
+ 
