3b925b786130c283c9c85c3267b7f3fd5bff653e
Hai Bui <brightc@hotmail.com>
2024-02-22 18:38:12 -0800

    Supplement more documentation.

--- calculator/src/main/java/com/velotronix/ChavviCalc.java
+++ calculator/src/main/java/com/velotronix/ChavviCalc.java
@@ -2,10 +2,16 @@ package com.velotronix;
 
 import java.util.*;
 
-
+/**
+ * Implementation of the Chavvi Calculator App.
+ * 
+ * 
+ * Author: Hai Bui
+ * Created: Feb 22, 2024
+ */
 public class ChavviCalc {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator();                
         
         Scanner scanner = new Scanner(System.in);
         calculator.run(scanner);
