f6699eb725659345b7ba757ec846187239f8ae43
Hai Bui <brightc@hotmail.com>
2024-04-04 17:51:03 -0700

    Complete implementation.

--- commander/src/main/java/com/velotronix/Commander.java
+++ commander/src/main/java/com/velotronix/Commander.java
@@ -1,61 +1,76 @@
 /**
- * Implementing and testing the Commander App.
+ * Implementing of the Commander class.
  * Author: Hai Bui
  * Created: April 3, 2024.
+ * 
  * Credits:
- * - Gabriel Solomon <Gabriel.Solomon@sjcc.edu>: The Command Menu, test data,
- *      and test code.
+ * - Gabriel Solomon <Gabriel.Solomon@sjcc.edu>: menu and command display code.
  */
 
 package com.velotronix;
 
+import java.util.Stack;
 import java.util.Random;
 import java.util.Scanner;
 
 import org.json.simple.*;
 
+/**
+ * The Commander class.
+ * - Can read a list of commands it can issue from a JSON file.
+ * - Can issue a random command from the command list.
+ * - Can undo the last command that was issued, down to the entire
+ *    command history as a stack until it's empty.
+ * - Can redo the last command that was issued, down to the entire
+ *    command history as a stack until it's empty.
+ */
 public class Commander {
   private Character command;
-  private String[] commands;
+  private String[] availableCommands;
+
+  private Stack<String> undoCommands;
+  private Stack<String> redoCommands;
 
   public Commander(String commandFilePath) {
     command = ' ';
-    commands = readCommands(commandFilePath);
+    readCommands(commandFilePath);
+
+    undoCommands = new Stack<String>();
+    redoCommands = new Stack<String>();
   }
 
   /**
    * Parse the JSON array to the String array.
    * 
-   * @param commandArray
-   * @return
+   * @param commandFilePath The complete file path to the JSON file that stores
+   *  a complete list of available commands.
    */
-  private String[] readCommands(String commandFilePath) {
+  private void readCommands(String commandFilePath) {
     JSONArray commandJSONArray = JSONFile.readArray(commandFilePath);
-    String[] arr = new String[commandJSONArray.size()];
+    availableCommands = new String[commandJSONArray.size()];
 
     // Get the names from JSON object
     for (int i = 0; i < commandJSONArray.size(); i++) {
       String command = commandJSONArray.get(i).toString();
-      arr[i] = command;
+      availableCommands[i] = command;
     }
-    return arr;
   }
 
   /**
    * Print the command array.
    * 
-   * @param commandArray
+   * @param commandArray The command collection to print.
    */
   private void printCommands() {
     System.out.printf("\nNumber\tCommand\n");
     System.out.printf("------\t---------------\n");
-    for (int i = 0; i < commands.length; i++) {
-      System.out.printf("%04d\t%s\n", i, commands[i]);
+    for (int i = 0; i < availableCommands.length; i++) {
+      System.out.printf("%04d\t%s\n", i, availableCommands[i]);
     }
   }
 
   /**
-   * Run the calculator. Display the menu, take the user's command inputs, and
+   * Display the menu, take the user's command inputs, and
    * performs the operations.
    * 
    * @param scanner Where to read the user's input.
@@ -81,13 +96,39 @@ public class Commander {
   /**
    * Print a menu command item.
    * 
-   * @param command     A one-character command.
+   * @param command A one-character command.
    * @param description The description of the command.
    */
   private void printMenuItem(Character command, String description) {
     System.out.printf("%s\t%s\n", command, description);
   }
 
+  /**
+   * Pop a command from the source stack and push that to the destination stack
+   * as part of the undo/redo mechanism. Also print out the command popped
+   * from the source stack.
+   * @param source The source stack to pop the command from.
+   * @param destination The destination stack to push the command to.
+   * @param messageHeader The header of the message that prints out the command.
+   * @param emptySourceMsg The message to print out if the source stack is empty.
+   */
+  private void shuffleCommand(
+    Stack<String> source, Stack<String> destination, String messageHeader,
+    String emptySourceMsg) {
+      if(source.size() > 0) {
+        String cmd = source.pop();
+        destination.push(cmd);
+
+        System.out.printf("[%s] %s\n", messageHeader, cmd);
+      }
+      else {
+        System.out.println(emptySourceMsg + " Issue a new command.");
+      }
+  }
+
+  /**
+   * Print out the user's menu.
+   */
   private void printMenu() {
     printMenuLine();
     System.out.println("\nGeneral Cavazos Commander App");
@@ -102,15 +143,19 @@ public class Commander {
   }
 
   /**
-   * Issue a random command
+   * Issue a command, taken at random from the available command collection.
+   * Also add that command to the undo command collection as part of the
+   * undo/redo mechanism.
    * 
-   * @param commandArray
-   * @param numCommand
    */
-  private void randomCommand() {
+  private void issueCommand() {
     Random rand = new Random();
-    int randIndex = rand.nextInt(commands.length);
-    System.out.printf("[COMMAND ISSUED]: %s\n", commands[randIndex]);
+    int randIndex = rand.nextInt(availableCommands.length);
+    
+    String command = availableCommands[randIndex];
+    undoCommands.push(command);
+
+    System.out.printf("[COMMAND ISSUED]: %s\n", command);
   }
 
   /**
@@ -143,14 +188,16 @@ public class Commander {
 
     switch (command) {
       case 'i':
-        randomCommand();
+        issueCommand();
         break;
       case 'l':
         printCommands();
         break;
       case 'u':
+        shuffleCommand(undoCommands, redoCommands, "UNDO COMMAND ISSUED", "ERROR: There is no command to undo.");
         break;
       case 'r':
+        shuffleCommand(redoCommands, undoCommands, "REDO COMMAND ISSUED", "ERROR: There is no command to redo.");
         break;
       case 'q':
         System.out.println("Thank you, General.\n");
--- commander/src/main/java/com/velotronix/CommanderApp.java
+++ commander/src/main/java/com/velotronix/CommanderApp.java
@@ -1,8 +1,22 @@
+/**
+ * The Commander App.
+ * Author: Hai Bui
+ * Created: April 3, 2024.
+ * 
+ * Credits:
+ * - Gabriel Solomon <Gabriel.Solomon@sjcc.edu>: menu and command display code.
+ */
+
 package com.velotronix;
 
 import java.util.Scanner;
 
-
+/**
+ * The Commander App.
+ * Initiate a Commander object, giving the path to a JSON file that contains all
+ *  the commands that can be issued. Then, display a user's menu and input
+ *  the user's commands.
+ */
 public class CommanderApp {
   public static void main(String[] args) {
     Commander commander = new Commander(
@@ -14,3 +28,4 @@ public class CommanderApp {
     scanner.close();
   }
 }
+
